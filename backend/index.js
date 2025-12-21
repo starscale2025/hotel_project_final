c1fae76f0f5c0c7cb0cf41e485cf09b08f5c6af6
Abhi <4bhiy23@gmail.com>
2025-12-21 20:19:07 +0530

    Admin Panel

--- backend/index.js
+++ backend/index.js
@@ -37,6 +37,7 @@ app.use(cors({
   origin: allowedOrigins,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
 }));
 
 
